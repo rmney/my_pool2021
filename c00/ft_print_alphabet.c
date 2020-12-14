3a365c737410d95f7e4de6226b1fdb20a4f3b6a6
rmney <55449742+rmney@users.noreply.github.com>
2020-12-14 18:28:15 +0100

    Update ft_print_alphabet.c

--- c00/ft_print_alphabet.c
+++ c00/ft_print_alphabet.c
@@ -1,16 +1,11 @@
-#include <unistd.h>
+
 void	ft_print_alphabet(char c)
 {
+	c = 'a';
 	
-	while(c <= 'z' && c >= 'a')
+	while(c <= 'z')
 	{
-		write(1, &c, 1);
-		c++;
+		write(1, &c. 1);
+			c++;
 	}
-}
-
-int	main()
-{
-	ft_print_alphabet('f');
-	return(0);
 }
