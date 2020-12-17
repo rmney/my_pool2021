5a5cbd27ea81a2824be9b0c2221004bcd3b9d359
rmney <riyadati.rm@gmail.com>
2020-12-17 18:16:03 +0100

    ft_atoi_base

---
+++ c04/ft_atoi_base.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void	 print_hex(int nb)
+{
+        char *str;
+        str = "0123456789abcdef";
+        if(nb == 0)
+                write(1, "0", 1);
+        while(nb)
+        {
+              	write(1, &str[nb % 16], 1);
+                nb /= 16;
+        }
+}
+
+int print_num(int decimalnum, int base)
+{
+    int octalnum = 0, temp = 1;
+
+    while (decimalnum != 0)
+    {
+        octalnum = octalnum + (decimalnum % base) * temp;
+        decimalnum = decimalnum / base;
+        temp = temp * 10;
+    }
+
+    return octalnum;
+}
+
+int	ft_atoi(char *str)
+{
+	int i;
+	int num;
+	int sign;
+
+	i = 0;
+	num = 0;
+	sign = 1;
+	while(str[i] == ' ' || str[i] == '\n' || str[i] == '\f' || str[i] == '\0')
+		i++;
+	if(str[i] == '+' || str[i] == '-')
+		if(str[i++] == '-')
+			sign = -1;
+	while(str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	return(sign * num);
+}
+
+void    ft_putnbr(int nb)
+{
+        char c;
+        if(nb < 0)
+        {
+                nb = -nb;
+                write(1, "-", 1);
+        }
+        if(nb < 10)
+        {
+                c = nb + '0';
+                write(1, &c, 1);
+        }
+        else
+        {
+                ft_putnbr(nb / 10);
+                ft_putnbr(nb % 10);
+        }
+}
+
+
+int     ft_atoi_base(char *str, char *base)
+{
+	int i;
+	int a = i;
+	
+	i = ft_atoi(str);
+       	if(ft_atoi(base) == 16)
+	{
+		print_hex(i);
+		return(i);
+	}
+	if(ft_atoi(base) == 10)
+	{
+		ft_putnbr(i);
+		return(i);
+	}
+	if(ft_atoi(base) == 8)
+	{
+		ft_putnbr(print_num(i, 8));
+		return(i);
+	}
+	if(ft_atoi(base) == 2)
+	{
+		ft_putnbr(print_num(i, 2));
+		return(i);
+	}
+	return(a);
+}
+
+int	main()
+{
+	ft_atoi_base("10", "16");
+	return(0);
+}
